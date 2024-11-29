61fb523924e2785bfd638f5eccf0f515ad847f84
bax082024 <bax082024@gmail.com>
2024-11-29 08:51:30 +0100

    added hash string 256 and display message

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace HashingDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Hashing Demo";
+            Console.WriteLine("Welcome to the Hashing Demo");
 
             // Get user input
             Console.WriteLine("Enter string to hash");
             string input = Console.ReadLine();
+
+            // Hash string usig SHA-256
+            string hashedString = ComputeSHA256Hash(input);
+
+            // Display hashed value
+            Console.WriteLine($"SHA-256 Hash: {hashedString}");
         }
 
     }
